bc32849a124cb3b3067b342ccd496e8f4d194da0
StivenYang <StivenYang626@gmail.com>
2019-07-11 00:58:24 +0800

    1

---
+++ src/main/java/top/hengshare/interviewer/thread/concurrency/HorseRace.java
@@ -0,0 +1,35 @@
+package top.hengshare.interviewer.thread.concurrency;
+
+/**
+ * @author StivenYang
+ * @program interview
+ * @description 赛马程序
+ * @date 2019-07-10 17:41
+ **/
+class Horse implements Runnable {
+    //计数器
+    //标识
+    //多个标识
+    //随机数种子
+    //循环栅栏
+    //构造函数
+    //获取多个标识
+
+    @Override
+    public void run() {
+
+    }
+    //重载toString方法
+    //tracks方法，跑马的进度
+
+}
+public class HorseRace {
+    //赛道长度
+    //马匹列表
+    //线程池
+    //循环栅栏，用来
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/top/hengshare/interviewer/thread/concurrency/PipedIO.java
@@ -0,0 +1,88 @@
+package top.hengshare.interviewer.thread.concurrency;
+
+import javafx.concurrent.Worker;
+
+import java.io.IOException;
+import java.io.PipedReader;
+import java.io.PipedWriter;
+import java.io.Reader;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author StivenYang
+ * @program interview
+ * @description 使用管道在线程间进行通信
+ * @date 2019-07-10 13:38
+ **/
+class Sender implements Runnable {
+    //生成一个随机数种子，隔随机时间向管道中输入数据
+    private Random random = new Random(47);
+    //新建一个管道写对象
+    private PipedWriter writer = new PipedWriter();
+
+    //获取管道属性
+    public PipedWriter getWriter() {
+        return writer;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                //向管道写入字符串
+                for (char c = 'A'; c < 'z'; c++) {
+                    writer.write(c);
+                }
+                //sleep一段时间
+                TimeUnit.MILLISECONDS.sleep(random.nextInt(500));
+            }
+        } catch (IOException e) {
+            System.out.println("io异常");
+        } catch (InterruptedException e) {
+            System.out.println("中断异常");
+        }
+    }
+}
+
+class Receiver implements Runnable {
+    //新建一管道读对象，从管道读取字符串
+    private PipedReader reader;
+
+    //构造方法，参数为写入管道，用来绑定对应的写入管道
+    public Receiver(Sender sender) throws IOException {
+        this.reader = new PipedReader(sender.getWriter());
+    }
+
+    @Override
+    public void run() {
+        //从读管道中读取字符串
+        try {
+            while (true) {
+                System.out.print((char) reader.read() + ",");
+            }
+        } catch (IOException e) {
+            System.out.println("reader io异常");
+        }
+    }
+}
+
+public class PipedIO {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        //新建一个发送者
+        Sender sender = new Sender();
+        //新建一个读取者
+        Receiver receiver = new Receiver(sender);
+        //启动发送者任务
+        ExecutorService exec = Executors.newCachedThreadPool();
+        exec.execute(sender);
+        //启动接受者任务
+        exec.execute(receiver);
+        //sleep
+        TimeUnit.SECONDS.sleep(4);
+        //shutdownnow
+        exec.shutdownNow();
+    }
+}
