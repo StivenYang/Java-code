7461d9377fd48347e666cab771c56bc4dca66412
StivenYang <StivenYang626@gmail.com>
2019-10-20 20:31:09 +0800

    备忘录模式

--- src/main/java/top/hengshare/interview/pattern/memento/struct/Originator.java
+++ src/main/java/top/hengshare/interview/pattern/memento/struct/Originator.java
@@ -8,17 +8,31 @@ package top.hengshare.interview.pattern.memento.struct;
  **/
 public class Originator {
 
+    /**
+     * 模拟状态
+     */
     private String state = "";
 
+    /**
+     * 创建备忘录
+     * @return
+     */
     public Memento createMemento() {
         return new MementoImpl(state);
     }
 
+    /**
+     * 设置备忘录
+     * @param memento
+     */
     public void setMemento(Memento memento){
         MementoImpl mementoImpl = (MementoImpl) memento;
         this.state = mementoImpl.getState();
     }
 
+    /**
+     * 具体的备忘录实现，可以通过这个备忘录保存当前的状态
+     */
     private static class MementoImpl implements Memento{
         private String state = "";
 
