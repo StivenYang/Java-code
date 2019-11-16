1294d3010a6fa85231af29491edc6ab81a13d542
StivenYang <StivenYang626@gmail.com>
2019-11-16 23:26:24 +0800

    随便写的一个单例

---
+++ src/main/java/top/hengshare/interview/generic/simple/TupleTest.java
@@ -0,0 +1,23 @@
+package top.hengshare.interview.generic.simple;
+
+/**
+ * @program: Java-Interview
+ * @description: TupleTest
+ * @author: StivenYang
+ * @create: 2019-11-16 13:51
+ **/
+public class TupleTest {
+
+    static TwoTuple<String, Integer> f(){
+        return new TwoTuple<>("hi", 47);
+    }
+
+    static ThreeTuple<Object, String, Integer> h(){
+        return new ThreeTuple<>(new Object(), "hi", 47);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(f());
+        System.out.println(h());
+    }
+}
---
+++ src/main/java/top/hengshare/interview/generic/simple/TwoTuple.java
@@ -0,0 +1,22 @@
+package top.hengshare.interview.generic.simple;
+
+/**
+ * @program: Java-Interview
+ * @description: 泛型，可以利用这个直接返回两个对象，从而省略创建对象的过程
+ * @author: StivenYang
+ * @create: 2019-11-16 13:40
+ **/
+public class TwoTuple<A, B> {
+    public final A first;
+    public final B second;
+
+    public TwoTuple(A first, B second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + first + ", " + second + ")";
+    }
+}
