01e898de8267bdb3b2154fa9cc39a89859024055
StivenYang <StivenYang626@gmail.com>
2020-07-18 00:07:43 +0800

    反转链表

---
+++ src/main/java/top/hengshare/interview/algorithm/leetcode/RevertList.java
@@ -0,0 +1,58 @@
+package top.hengshare.interview.algorithm.leetcode;
+
+/**
+ * @author StevenYang
+ * @description 翻转单链表  leetCode：206
+ * @date 2020/7/16
+ **/
+public class RevertList {
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = null;
+        print(node1);
+        ListNode revert = revert(node1);
+        print(revert);
+    }
+
+    public static void print(ListNode node){
+        ListNode listNode;
+        if (node == null) {
+            return;
+        }
+        System.out.println(node.val);
+        listNode = node.next;
+        print(listNode);
+    }
+
+    private static ListNode revert(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode nextTemp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = nextTemp;
+        }
+        System.out.println();
+        return pre;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+
+    @Override
+    public String toString() {
+        return val+"";
+    }
+}
--- src/main/java/top/hengshare/interview/algorithm/sort/Quick.java
+++ src/main/java/top/hengshare/interview/algorithm/sort/Quick.java
@@ -12,30 +12,35 @@ public class Quick {
     public static int[] arr = {1, 3, 5, 2, 11, 23, 34, 31, 12, 11};
     public static int count = 0;
 
-    public static void sort(int[] arr, int l, int r) {
-        if (l < r) {
+    public static void sort(int[] arr, int left, int right) {
+        if (left < right) {
             int i, j, x;
-            i = l;
-            j = r;
+            i = left;
+            j = right;
             x = arr[i];
             while (i < j) {
+                //有一个左值针和右指针，如果左值针和右指针没有重合
                 while (i < j && arr[j] > x) {
-                    //右边向左找第一个小于x的元素
+                    //如果左值针和右指针没有重合，且右指针指向的元素大于左指针指向的元素，
+                    // 让右指针往左移，直到碰见右指针指向的元素小于或者等于左值针的元素
                     j--;
                 }
-                if (i < j) {//
+                if (i < j) {
+                    //如果左值针和右指针没有重合，就让右指针指向的元素的值赋给左指针指向的元素，并将左值针向右移动一位
                     arr[i++] = arr[j];
                 }
                 while (i < j && arr[i] < x) {
+                    //如果左值针和右指针没有重合，且左值针指向的元素小于左值针指向的元素，将左值针右移一位
                     i++;
                 }
                 if (i < j) {
+                    //如果本轮还没完，就将右指针左移一位，并交换左右指针指向的值
                     arr[j--] = arr[i];
                 }
             }
             arr[i] = x;
-            sort(arr, l, i - 1);
-            sort(arr, i + 1, r);
+            sort(arr, left, i - 1);
+            sort(arr, i + 1, right);
         }
     }
 
