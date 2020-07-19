9c7fd2fbd8eedfb6ddaa6434b5245b38dd426ece
StivenYang <StivenYang626@gmail.com>
2020-07-19 11:05:36 +0800

    队列和栈互相实现

---
+++ src/main/java/top/hengshare/interview/algorithm/leetcode/QueueToStack.java
@@ -0,0 +1,54 @@
+package top.hengshare.interview.algorithm.leetcode;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+/**
+ * @author StevenYang
+ * @description 用队列实现栈
+ * @date  2020/7/18
+ **/
+public class QueueToStack {
+
+    private Queue<Integer> input;
+    private Queue<Integer> output;
+    int top;
+
+    /** Initialize your data structure here. */
+    public QueueToStack() {
+        input = new ArrayDeque<>();
+        output = new ArrayDeque<>();
+    }
+
+    /** Push element x onto stack. */
+    public void push(int x) {
+        input.add(x);
+        top = x;
+        while (!output.isEmpty()) {
+            input.add(output.remove());
+        }
+        Queue<Integer> temp = output;
+        output = input;
+        input = temp;
+    }
+
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        output.remove();
+        int res = top;
+        if (!output.isEmpty()) {
+            top = output.peek();
+        }
+        return res;
+    }
+
+    /** Get the top element. */
+    public int top() {
+        return top;
+    }
+
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return output.isEmpty();
+    }
+}
---
+++ src/main/java/top/hengshare/interview/algorithm/leetcode/StackToQueue.java
@@ -0,0 +1,50 @@
+package top.hengshare.interview.algorithm.leetcode;
+
+import top.hengshare.interview.utils.net.util.Stack;
+
+/**
+ * @author StevenYang
+ * @description 用栈实现队列的功能
+ * @date  2020/7/18
+ **/
+public class StackToQueue {
+
+    private Stack<Integer> input;
+    private Stack<Integer> output;
+
+    /** Initialize your data structure here. */
+    public StackToQueue() {
+        input = new Stack<>();
+        output = new Stack<>();
+    }
+
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        input.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        if (output.empty()) {
+            while (!input.empty()) {
+                output.push(input.pop());
+            }
+        }
+        return output.pop();
+    }
+
+    /** Get the front element. */
+    public int peek() {
+        if (output.empty()) {
+            while (!input.empty()) {
+               output.push(input.pop());
+            }
+        }
+        return output.peek();
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return input.empty() && output.empty();
+    }
+}
