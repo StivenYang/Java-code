ac270ebd73885cfa3883ff5f9eb117a17163a973
StivenYang <StivenYang626@gmail.com>
2020-10-16 15:25:41 +0800

    提交算法

---
+++ src/main/java/top/hengshare/interview/algorithm/leetcode/P696.java
@@ -0,0 +1,38 @@
+package top.hengshare.interview.algorithm.leetcode;
+
+/**
+ * P696 计数二进制子串
+ *
+ * @author v_yangjiaheng_dxm
+ * @date 2020/10/14 8:59 下午
+ **/
+public class P696 {
+    public static int countBinarySubstrings(String s) {
+        String[] arr = s.split("");
+        int len = arr.length;
+        int count = 0;
+
+        for(int i = 0; i<len-1; i++){
+            String left = arr[i];
+            String right = arr[i+1];
+            int leftIndex = i;
+            int rightIndex = i+1;
+
+            if (!left.equals(right)) {
+                while (leftIndex >= 0 && rightIndex <= len - 1) {
+                    if (arr[leftIndex].equals(left) && arr[rightIndex].equals(right)) {
+                        count++;
+                        leftIndex--;
+                        rightIndex++;
+                    }else break;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        String a = "1010001";
+        System.out.println(countBinarySubstrings(a));
+    }
+}
